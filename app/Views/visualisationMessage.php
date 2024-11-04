138f8b736db23923a28ad4e16371e95dea50f811
fluky <olivacedric@hotmail.fr>
2024-11-04 16:26:07 +0100

    visualisation changement

--- app/Views/visualisationMessage.php
+++ app/Views/visualisationMessage.php
@@ -12,19 +12,30 @@ if (!$session->has('user')) {
 <?= $this->extend('layout') ?>
 
 <?= $this->section('content') ?>
-    <link rel="stylesheet" href="<?php echo base_url('css/styleCreate.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('css/styleVisu.css'); ?>">
     <h2>Visualisation de l'évènement</h2>
 
-    <div class="container">
-        <div class="form-group">
-            <label for="titre">Titre</label>
-            <input type="text" id="titre" value="<?= $message['Title'] ?>" readonly>
+    <div class="container-wrapper">
+
+        <div class="containerUn">
+            <div class="form-group">
+                <input type="text" id="titre" value="<?= $message['Title'] ?>" readonly>
+            </div>
+
+            <div class="form-group">
+                <textarea id="description" readonly><?= $message['Text'] ?></textarea>
+            </div>
+        </div>
+        <div class="containerDeux">
+            <div class="form-group">
+                <input type="text" id="titre" value="<?= $message['Title'] ?>" readonly>
+            </div>
+
+            <div class="form-group">
+                <textarea id="description" readonly><?= $message['Text'] ?></textarea>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label for="description" class="move-up">Description évènement</label>
-            <textarea id="description" readonly><?= $message['Text'] ?></textarea>
-        </div>
     </div>
 <?= $this->endSection() ?>
 </body>
