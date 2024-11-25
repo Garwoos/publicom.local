2b14a9e2f9b212d2e91e02067f7b3757602783a6
fluky <olivacedric@hotmail.fr>
2024-11-25 15:59:51 +0100

    cssshistori

--- app/Views/history.php
+++ app/Views/history.php
@@ -1,25 +1,26 @@
 <?= $this->extend('layout') ?>
 
 <?= $this->section('content') ?>
+    <link rel="stylesheet" href="<?php echo base_url('css/style.css'); ?>">
     <h2>Historique</h2>
     <table class="table">
         <thead>
             <tr>
                 <th>idHistorique</th>
                 <th>idMessage</th>
-                <th>mailUser</th>
-                <th>oldMessage</th>
+                <th>Mail</th>
+                <th>Anciens messages</th>
                 <th>Date</th>
             </tr>
         </thead>
         <tbody>
             <?php foreach ($data as $row) : ?>
                 <tr>
-                    <td><?= $row['idModification'] ?></td>
-                    <td><?= $row['IdMessage'] ?></td>
-                    <td><?= $row['mailUser'] ?></td>
-                    <td><?= $row['oldMessage'] ?></td>
-                    <td><?= $row['Date'] ?></td>
+                    <td class="centrer"><?= $row['idModification'] ?></td>
+                    <td class="centrer"><?= $row['IdMessage'] ?></td>
+                    <td class="centrer"><?= $row['mailUser'] ?></td>
+                    <td class="centrer"><?= $row['oldMessage'] ?></td>
+                    <td class="centrer"><?= $row['Date'] ?></td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
